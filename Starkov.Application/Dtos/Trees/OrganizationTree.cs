be341df2056b53e51db71581cf60c315041a49a2
artur <zeroone02@mail.ru>
2023-11-30 17:44:04 +0300

    bfs

--- Starkov.Application/Dtos/Trees/OrganizationTree.cs
+++ Starkov.Application/Dtos/Trees/OrganizationTree.cs
@@ -4,4 +4,28 @@ namespace Starkov.Application.Dtos.Trees;
 public class OrganizationTree
 {
     public Collection<DepartmentTreeItem> Departments { get; set; }
+    public DepartmentTreeItem GetDepartment(int id)
+    {
+        var queue = new Queue<DepartmentTreeItem>();
+
+        foreach (var item in Departments)
+        {
+            queue.Enqueue(item);
+        }
+
+        while (queue.Any())
+        {
+            var department = queue.Dequeue();
+            if (department.Id == id)
+            {
+                return department;
+            }
+            foreach (var item in department.Departments)
+            {
+                queue.Enqueue(item);
+            }
+        }
+
+        return default;
+    }
 }
